d5f438f853fea17f51e80bf6e4511e205f61a6e9
amel <amelovitsh@gmail.com>
2017-02-03 03:06:20 +0100

    *bloquée

--- horloge.php
+++ horloge.php
@@ -6,4 +6,16 @@ ex:
 // =>  "Dans une seconde, il sera 23 heure(s), 13 minute(s) et 0 seconde(s)".`
 
 */
+$time=date(H,i,s);
+
+function nextSecond($H,$i,$S){
+
+	$H=date("H");
+	$i=date("i");
+	$S=date("s");
+
+	 $next=$time+mktime(date("H"),date("i"),date("s")+1);
+	 return $next;
+}
+echo "Dans une seconde, il sera " .$next;
 ?>
